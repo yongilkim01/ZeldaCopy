35d4658481fd19157cde0a1b08f273da7414efaa
yongilkim01 <yongil.k01@gmail.com>
2024-10-30 19:50:57 +0900

    chore: update engine core comments

--- LinkToThePast/EngineCore/EngineAPICore.cpp
+++ LinkToThePast/EngineCore/EngineAPICore.cpp
@@ -20,33 +20,40 @@ UEngineAPICore::UEngineAPICore()
 
 UEngineAPICore::~UEngineAPICore()
 {
-	std::map<std::string, class ULevel*>::iterator StartIter = Levels.begin();
-	std::map<std::string, class ULevel*>::iterator EndIter = Levels.end();
-
-	for (; StartIter != EndIter; ++StartIter)
+	// 저장하고 있던 레벨들의 메모리 해제
 	{
-		if (nullptr != StartIter->second)
-		{
-			delete StartIter->second;
-			StartIter->second = nullptr;
-		}
-	}
+		std::map<std::string, class ULevel*>::iterator StartIter = Levels.begin();
+		std::map<std::string, class ULevel*>::iterator EndIter = Levels.end();
 
-	Levels.clear();
+		for (; StartIter != EndIter; ++StartIter)
+		{
+			if (nullptr != StartIter->second)
+			{
+				delete StartIter->second;
+				StartIter->second = nullptr;
+			}
+		}
+		Levels.clear();
+	}
 }
 
 
-int UEngineAPICore::EngineStart(HINSTANCE _Inst, UContentsCore* _UserCore)
+int UEngineAPICore::EngineStart(HINSTANCE hInstance, UContentsCore* ContentsCore)
 {
+	// 메모리 릭 체크 설정
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	UserCore = _UserCore;
+	// 함수 인자로 전달받은 유저 코어를 엔진 코어의 유저 코어에게 할당
+	UserCore = ContentsCore;
 
-	UEngineWindow::EngineWindowInit(_Inst);
+	// 함수 인자로 전달받은 HInstance 핸들러로 윈도우 초기화
+	UEngineWindow::EngineWindowInit(hInstance);
 
-	// 객체 안만들면 객체지향이 아닌거 같아서 객체로 하자.
+	// 엔진 코어 객체 생성
 	UEngineAPICore Core = UEngineAPICore();
+	// 엔진 코어 객체의 윈도우 오픈
 	Core.EngineMainWindow.Open();
+	// 전역 정적 코어 변수에 생성된 엔진 코어의 주소값을 할당
 	MainCore = &Core;
 
 	EngineDelegate Start = EngineDelegate(std::bind(EngineBeginPlay));
