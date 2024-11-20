79f5d298743e4cc1b77bed408e6990104d82ba60
yongilkim01 <yongil.k01@gmail.com>
2024-11-20 19:30:27 +0900

    chore: delete comments player debug

--- LinkToThePast/Zelda/PlayerCharacter_3.Debug.cpp
+++ LinkToThePast/Zelda/PlayerCharacter_3.Debug.cpp
@@ -14,23 +14,8 @@ void APlayerCharacter::PrintDebugInfo(float DeltaTime)
 	UEngineDebug::CoreOutPutString("FPS : " + std::to_string(1.0f / DeltaTime));
 	UEngineDebug::CoreOutPutString("Location : " + GetActorLocation().ToString(), GetDebugLocation(0));
 	UEngineDebug::CoreOutPutString("Level : " + GetWorld()->GetName(), GetDebugLocation(1));
-	//UEngineDebug::CoreOutPutString("PlayerScale : " + GetTransform().Scale.ToString());
-	//UEngineDebug::CoreOutPutString("PlayerLefTop : " + GetTransform().CenterLeftTop().ToString());
-	//UEngineDebug::CoreOutPutString("Player Room Name : " + CurRoom->GetName());
-	//UEngineDebug::CoreOutPutString("Player Room Collision Name : " + this->CollisionImage->GetName());
-
-			// 현재 플레이어의 위치
-	FVector2D PlayerLocation = GetActorLocation() - GetCurRoom()->GetActorLocation();
-	// 현재 플레이어의 위치에 있는 콜리전 색상
-	UColor Color = GetCollisionImage()->GetColor(PlayerLocation);
-
-	//UEngineDebug::CoreOutPutString("Current RGB : " + Color.ToString());
 	UEngineDebug::CoreOutPutString("Room : " + GetCurRoom()->GetName(), GetDebugLocation(2));
-	//UEngineDebug::CoreOutPutString("Current Room Collision Name : " + GetCollisionImage()->GetName(), GetNoneCameraLocation());
-
 	UEngineDebug::CoreOutPutString("HP : " + std::to_string(this->CurrentHP), GetDebugLocation(3));
-	//UEngineDebug::CoreOutPutString("Player order : " + std::to_string(this->SpriteRenderer->GetOrder()));
-	//UEngineDebug::CoreOutPutString("KncokBack DeltaTime: " + std::to_string(KnockBackTime));
 
 	if (true == HitCollision->IsActive())
 	{
